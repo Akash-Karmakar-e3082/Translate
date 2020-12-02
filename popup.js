b01ecae14349119f5d0a8f63d223729004930d39
Akash karmakar <akash.karmakar@freshworks.com>
2020-12-03 00:51:04 +0530

    added translation endpoint and button

--- popup.js
+++ popup.js
@@ -9,7 +9,24 @@ $(function()
  		console.log(requestURL);
  		console.log(language);
  		var d={lang: language, url: requestURL}
-		$.post( "http://localhost:5050",d)
+		$.post( "http://localhost:5000/calculate_valid_translations",d)
+		.done(function(data){
+			console.log("done")
+			$('#load').css('display','none');
+			$('#insert').append(data);
+			console.log(data);})
+		.fail(function(data){alert("OH NO Failed!!")})
+
+	});
+	$('#translate').click(function(){
+		$('#insert').empty();
+		$('#load').css('display','inline');
+ 		requestURL = $('#url').val();
+ 		language = $('#language').val();
+ 		console.log(requestURL);
+ 		console.log(language);
+ 		var d={lang: language, url: requestURL}
+		$.post( "http://localhost:5000/translate",d)
 		.done(function(data){
 			console.log("done")
 			$('#load').css('display','none');
