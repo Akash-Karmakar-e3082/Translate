9af15549721ae6ceb687da91be483ceb9c87d789
Akash karmakar <akash.karmakar@freshworks.com>
2020-12-03 11:02:20 +0530

    code clean up and refactoring

--- popup.js
+++ popup.js
@@ -1,38 +1,23 @@
 $(function()
 {
 	$('#bttn').click(function(){
-		//http://localhost:5050
 		$('#insert').empty();
 		$('#load').css('display','inline');
  		requestURL = $('#url').val();
  		language = $('#language').val();
- 		console.log(requestURL);
- 		console.log(language);
  		var d={lang: language, url: requestURL}
-		$.post( "http://localhost:5000/calculate_valid_translations",d)
+		$.get( "http://localhost:5000/calculate_valid_translations?dest_lang="+language+"&url="+requestURL)
 		.done(function(data){
-			console.log("done")
 			$('#load').css('display','none');
-			$('#insert').append(data);
-			console.log(data);})
+			$('#insert').append(data);})
 		.fail(function(data){alert("OH NO Failed!!")})
 
 	});
 	$('#translate').click(function(){
 		$('#insert').empty();
-		$('#load').css('display','inline');
  		requestURL = $('#url').val();
  		language = $('#language').val();
- 		console.log(requestURL);
- 		console.log(language);
- 		var d={lang: language, url: requestURL}
-		$.post( "http://localhost:5000/translate",d)
-		.done(function(data){
-			console.log("done")
-			$('#load').css('display','none');
-			$('#insert').append(data);
-			console.log(data);})
-		.fail(function(data){alert("OH NO Failed!!")})
+ 		chrome.tabs.create({ url: "http://localhost:5000/translate?dest_lang="+language+"&url="+requestURL })
 
 	});
 });
